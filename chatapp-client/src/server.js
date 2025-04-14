ce564640d5ac8059b273ae7dc63f0affb3ce89d8
chae0Park <cypark022@gmail.com>
2025-04-15 01:10:56 +0900

    process.env added

--- chatapp-client/src/server.js
+++ chatapp-client/src/server.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'; //library 
-const socket = io(`${process.env.REACT_APP_SERVER_URL}`);
+const socket = io(process.env.REACT_APP_SERVER_URL);
 export default socket;
 
 /*server.js**에서 socket.io-client를 사용하여 클라이언트 측 socket 객체를 생성하고 있습니다 */
